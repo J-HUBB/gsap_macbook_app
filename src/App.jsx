24f3db13b54b491d5b37888aa4ea348aae23d9ad
J-HUBB <lowlifeenterprizes@yahoo.com>
2026-01-28 18:35:33 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+return (
+ <main className="text-3xl font-bold underline text-indigo-700">
+  <Navbar/>
+ </main>
+ );
+ };
+
+ export default App;
