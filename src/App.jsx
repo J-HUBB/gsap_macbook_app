91b86c695e3ac89fb98c550e2997457ed2a9432a
J-HUBB <lowlifeenterprizes@yahoo.com>
2026-02-03 19:26:53 -0500

    Implemented Performance section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import Navbar from "./components/Navbar.jsx";
+import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
+
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
-return (
- <main>
-  <Navbar/>
-    <Hero />
- </main>
- );
- };
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
+}
 
- export default App;
+export default App
